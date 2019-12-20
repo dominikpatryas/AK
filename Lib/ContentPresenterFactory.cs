7333b66d316f96653fd4ce13b7a83d0461c183c6
Dominik Patryas <dominik.patryas@gmail.com>
2019-12-20 21:57:58 +0100

    init

---
+++ Lib/ContentPresenterFactory.cs
@@ -0,0 +1,56 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+namespace Graphviz4Net.WPF
+{
+
+
+    class ContentPresenterFactory : IWPFLayoutElementsFactory
+    {
+        public FrameworkElement CreateVertex(object vertex)
+        {
+            return new ContentPresenter { Content = vertex };
+        }
+
+        public FrameworkElement CreateEdgeArrow(EdgeArrowViewModel arrowViewModel)
+        {
+            if (arrowViewModel.Arrow == null)
+            {
+                return new Line
+                {
+                    X1 = 0,
+                    Y1 = 0,
+                    X2 = 0,
+                    Y2 = 10,
+                    Stroke = new SolidColorBrush(Colors.Black),
+                    StrokeThickness = 1
+                };
+            }
+            else
+            {
+                return new ContentPresenter { Content = arrowViewModel.Arrow };                
+            }
+        }
+
+        public FrameworkElement CreateSubGraphBorder(BorderViewModel borderViewModel)
+        {
+            return new ContentPresenter { Content = borderViewModel };
+        }
+
+        public FrameworkElement CreateEdge(EdgeViewModel edgeViewModel)
+        {
+            return new ContentPresenter { Content = edgeViewModel };
+        }
+
+        public FrameworkElement CreateEdgeLabel(EdgeLabelViewModel labelViewModel)
+        {
+            return new ContentPresenter { Content = labelViewModel };
+        }
+
+        public FrameworkElement CreateEdgeArrowLabel(EdgeArrowLabelViewModel labelViewModel)
+        {
+            return new ContentPresenter { Content = labelViewModel };
+        }
+    }
+}
